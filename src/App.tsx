8def7644476c158efe252d3901019bb8ff08bdb7
michonchy <mintan0222@gmail.com>
2022-05-13 19:37:33 +0900

    配色決め

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Box, Button, createTheme, ThemeProvider } from '@mui/material';
+import { AppBar, Box, Button, createTheme, Tab, Tabs, ThemeProvider, Toolbar } from '@mui/material';
 import { Input } from '@mui/material';
 import AWS from "aws-sdk";
 import axios from 'axios';
+import fileDownload from 'js-file-download';
+import { orange, grey } from '@mui/material/colors';
 
 
 AWS.config.update({
@@ -17,9 +19,58 @@ const s3 = new AWS.S3({
   region: 'ap-northeast-1',
 })
 
-const theme = createTheme();
-function App() {
+function TabPanel(props: {
+  value: number,
+  index: number,
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {
+        props.value === props.index && (
+          props.children
+        )
+      }
+    </div>
+  )
+}
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+function UploadView() {
   const [file, setFile] = useState(undefined as (File | undefined));
+  return (
+    <div>
+      <Box sx={{
+        height: '50vh'
+      }}>
+            <Input onChange={ (event) => {
+              setFile(change_input(event)) ; // ❶
+            }} type="file" name="file1" id="file1" /><br /><br />
+          <button onClick={() => {
+            if (file) {
+              upload(file); // ❷
+            }
+          }} 
+          // sx={{
+          //   size: "large",
+          //   color: "#E7D5E5", // # 赤(0〜255) 緑(0〜255) 青(0〜255) => #00FF00 RedGreenBlue
+          //   backgroundColor: "#FFFFFF",
+          //   fontSize: 30,
+          //   fontWeight: "bold",
+          //   "&:hover": {
+          //     background: "#E7D5E5"
+          //   },
+          // }}
+          >Upload</button>
+          </Box>
+    </div>
+  )
+}
+function MusicListView() {
   const [musics, setMusics] = useState([] as Array<string>);
   const musicURL = "https://9u3kgnfgt1.execute-api.ap-northeast-1.amazonaws.com/development/music"
   useEffect(() => {
@@ -29,39 +80,88 @@ function App() {
     });
   }, []);
   return (
+    <Box sx={{
+      height: '50vh'
+    }}>
+      {
+            musics.map((music) => {
+              return (
+                <Box key={music}><Button onClick={() => {
+                  console.log(music)
+                  s3.getObject({
+                    Bucket:'melody-api-development-movie-contents',
+                    Key:music,
+                  }, (err: AWS.AWSError, data: AWS.S3.GetObjectOutput) => {
+                    console.log(data.Body)
+                    if (data.Body === undefined) {
+                      return;
+                    }
+                    fileDownload(data.Body as ArrayBuffer,music)
+                  })
+                }}>{music}</Button></Box>
+              )
+            })
+          }
+    </Box>
+  )
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[900],
+    },
+    secondary: {
+      main: 'white',
+      contrastText: '#ffcc00',
+    },
+    text: {
+      primary: grey[900],
+      secondary: grey[800],
+    },
+  }
+});
+function App() {
+  const [tabIndex,setTabIndex] = useState(0)
+  return (
     <ThemeProvider theme={theme} >
+      <AppBar>
+      <Box display="flex">
+      <Toolbar>Melody App</Toolbar>
+      <Tabs variant="fullWidth" textColor="secondary" value={tabIndex} sx={{
+        accentColor: "yellow"
+      }} onChange={(_, newValue: number) => {
+              setTabIndex(newValue);
+            }} aria-label="basic tabs example">
+              <Tab sx={{
+                color: "red"
+              }} label="Item One" {...a11yProps(0)} />
+              <Tab sx={{
+                
+                color: "red"
+              }}  label="Item Two" {...a11yProps(1)} />
+            </Tabs>
+      </Box>
+      </AppBar>
       <Box justifyContent={"center"} alignItems={"center"} sx={{
         display: 'flex',
         backgroundColor: "#DAC7C8",
         height: '100vh'
       }}>
         <Box flexDirection={"column"}>
-          <Box>
-            <Input onChange={ (event) => {
-              setFile(change_input(event)) ; // ❶
-            }} type="file" name="file1" id="file1" /><br /><br />
+          <Box sx={{ 
+            borderBottom: 1, 
+            borderColor: 'divider',
+            width: '100%'
+          }}>
+            
           </Box>
-          <Button onClick={() => {
-            if (file) {
-              upload(file); // ❷
-            }
-          }} variant="contained" sx={{
-            size: "large",
-            color: "#E7D5E5", // # 赤(0〜255) 緑(0〜255) 青(0〜255) => #00FF00 RedGreenBlue
-            backgroundColor: "#FFFFFF",
-            fontSize: 30,
-            fontWeight: "bold",
-            "&:hover": {
-              background: "#E7D5E5"
-            },
-          }}>Upload</Button>
-          {
-            musics.map((music) => {
-              return (
-                <li key={music}>{music}</li>
-              )
-            })
-          }
+          <TabPanel value={tabIndex} index={0}>
+            {UploadView()}
+          </TabPanel>
+          <TabPanel value={tabIndex} index={1}>
+            {MusicListView()}
+          </TabPanel>
         </Box>
         
       </Box>
