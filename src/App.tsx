a8bf69db3c5041a0707dfb0388a6bddb71306422
michonchy <mintan0222@gmail.com>
2022-07-21 19:48:29 +0900

    logの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AppBar, Box, Button, createTheme, Tab, Tabs, ThemeProvider, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, createTheme, Tab, Tabs, ThemeProvider, Toolbar,Alert } from '@mui/material';
 import { Input } from '@mui/material';
 import AWS from "aws-sdk";
 import axios from 'axios';
@@ -42,17 +42,21 @@ function a11yProps(index: number) {
 }
 function UploadView() {
   const [file, setFile] = useState(undefined as (File | undefined));
+  const [status, setStatus] = useState("");
   return (
     <div>
       <Box sx={{
         height: '50vh'
       }}>
+        <Alert>{status}</Alert>
             <Input onChange={ (event) => {
+              setStatus(event.target.toString());
               setFile(change_input(event)) ; // ❶
             }} type="file" name="file1" id="file1" /><br /><br />
-          <button onClick={() => {
+          <Button variant="contained" component="span" onClick={async () => {
             if (file) {
-              upload(file); // ❷
+              const result = await upload(file);
+              setStatus(result.toString());
             }
           }} 
           // sx={{
@@ -65,13 +69,14 @@ function UploadView() {
           //     background: "#E7D5E5"
           //   },
           // }}
-          >Upload</button>
+          >Upload</Button>
           </Box>
     </div>
   )
 }
 function MusicListView() {
   const [musics, setMusics] = useState([] as Array<string>);
+  const [status, setStatus] = useState("");
   const musicURL = "https://9u3kgnfgt1.execute-api.ap-northeast-1.amazonaws.com/development/music"
   useEffect(() => {
     axios.get(musicURL).then((response) => {
@@ -83,19 +88,26 @@ function MusicListView() {
     <Box sx={{
       height: '50vh'
     }}>
+      <Alert>{status}</Alert>
       {
             musics.map((music) => {
               return (
                 <Box key={music}><Button onClick={() => {
                   console.log(music)
+                  setStatus(music);
                   s3.getObject({
                     Bucket:'melody-api-development-movie-contents',
                     Key:music,
                   }, (err: AWS.AWSError, data: AWS.S3.GetObjectOutput) => {
                     console.log(data.Body)
+                    if (err.message.length !== 0){
+                      setStatus(err.message);
+                      return;
+                    }
                     if (data.Body === undefined) {
                       return;
                     }
+                    setStatus("Success")
                     fileDownload(data.Body as ArrayBuffer,music)
                   })
                 }}>{music}</Button></Box>
@@ -126,22 +138,24 @@ function App() {
   return (
     <ThemeProvider theme={theme} >
       <AppBar>
-      <Box display="flex">
-      <Toolbar>Melody App</Toolbar>
-      <Tabs variant="fullWidth" textColor="secondary" value={tabIndex} sx={{
-        accentColor: "yellow"
-      }} onChange={(_, newValue: number) => {
-              setTabIndex(newValue);
-            }} aria-label="basic tabs example">
-              <Tab sx={{
-                color: "#aaa"
-              }} label="Item One" {...a11yProps(0)} />
-              <Tab sx={{
-                
-                color: "#aaa"
-              }}  label="Item Two" {...a11yProps(1)} />
-            </Tabs>
-      </Box>
+        <Box display="flex" sx={{
+          alignItems: "center"
+        }}>
+          <Toolbar>Melody App</Toolbar>
+          <Tabs variant="fullWidth" textColor="secondary" value={tabIndex} sx={{
+            accentColor: "yellow"
+          }} onChange={(_, newValue: number) => {
+            setTabIndex(newValue);
+          }} aria-label="basic tabs example">
+            <Tab sx={{
+              color: "#aaa"
+            }} label="UPLOAD" {...a11yProps(0)} />
+            <Tab sx={{
+              
+              color: "#aaa"
+            }}  label="MUSICS" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
       </AppBar>
       <Box justifyContent={"center"} alignItems={"center"} sx={{
         display: 'flex',
@@ -149,13 +163,6 @@ function App() {
         height: '100vh'
       }}>
         <Box flexDirection={"column"}>
-          <Box sx={{ 
-            borderBottom: 1, 
-            borderColor: 'divider',
-            width: '100%'
-          }}>
-            
-          </Box>
           <TabPanel value={tabIndex} index={0}>
             {UploadView()}
           </TabPanel>
@@ -171,16 +178,23 @@ function App() {
 
 export default App;
 
-function upload(file: File) {
+function upload(file: File): Promise<AWS.S3.ManagedUpload.SendData> {
   console.log("OK",file);
-  s3.upload({
-    Bucket: "melody-api-development-movie-contents",
-    Key: "movie/"+file.name, 
-    Body: file,
-    ACL: "public-read"
-  }, (error, data) => {
-    console.log({data});
-    console.log({error});
+  return new Promise((resolve, reject) => {
+    s3.upload({
+      Bucket: "melody-api-development-movie-contents",
+      Key: "movie/"+file.name, 
+      Body: file,
+      ACL: "public-read"
+    }, (error, data) => {
+      if (error !== undefined) {
+        console.log({error});
+        reject(error);
+      } else {
+        console.log({data});
+        resolve(data);
+      }
+    });
   });
 }
 
